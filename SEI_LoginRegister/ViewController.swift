2336e2ffa08fe28bab13ece70658a6085594d302
Marko Milosavljevic <milo.marko@icloud.com>
2021-06-26 17:54:34 +0200

    Initial Commit

---
+++ SEI_LoginRegister/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SEI_LoginRegister
+//
+//  Created by Marko Milosavljevic on 26.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
